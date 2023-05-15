d4fb0283a661b3c5ded996536f4b1e0568b41084
Kevin Schneider <schneider.kev@outlook.de>
2023-05-15 14:42:02 +0200

    Init projects

---
+++ src/FsLab.Interactive/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace FsLab.Interactive
+
+open Deedle.Interactive
+open FSharp.Stats.Interactive
+open Plotly.NET.Interactive
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ src/FsLab/Library.fs
@@ -0,0 +1,9 @@
+﻿namespace FsLab
+
+open Deedle
+open FSharp.Stats
+open Plotly.NET
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
